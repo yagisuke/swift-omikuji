160711e164f57ab383fc1ea3ec4345412ddce303
yagisuke <yagisuke.0209@gmail.com>
2018-08-06 01:24:09 +0900

    lesson2

---
+++ app/app/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  app
+//
+//  Created by yagisuke on 2018/08/06.
+//  Copyright © 2018 yagisuke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var result: UILabel!
+    @IBAction func getOmikuji(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
